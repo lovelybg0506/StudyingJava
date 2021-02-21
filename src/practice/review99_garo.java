58a0086716ed2075b61e061a9c1017b76f028e8c
lovelybg0506 <lovelybg0506@gmail.com>
2021-02-21 12:40:46 +0900

    0

---
+++ src/practice/review99_garo.java
@@ -0,0 +1,21 @@
+package practice;
+
+public class review99_garo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i=2;i<10;i++) {
+			System.out.print("[구구단"+i+"단]\t");
+		}
+		System.out.println();
+		
+		for(int i=1;i<10;i++) {
+		for(int j=2;j<10;j++) {
+			System.out.print(j+"*"+i+"="+j*i+("\t\t"));
+		}
+		System.out.println();
+	}
+		
+	}
+}
---
+++ src/practice/review99_sero.java
@@ -0,0 +1,22 @@
+package practice;
+
+public class review99_sero {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i=2;i<10;i++) {								//备备窜 技肺.
+			System.out.println("备备窜"+i+"窜");
+		
+		for(int j=1;j<10;j++) {
+			System.out.print(i+"*"+j+"="+i*j);
+			System.out.println();
+		}
+
+		System.out.println();
+	}
+	}
+}
+
+

d911d9d938906f0918783e484966f2a175f9240e
lovelybg0506 <lovelybg0506@gmail.com>
2021-02-21 23:55:21 +0900

    add modiffication

--- src/practice/java6.java
+++ src/practice/java6.java
@@ -3,34 +3,66 @@ package practice;
 public class java6 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub				//배열
-		String a = "a";
-		int b = 1;
+		// TODO Auto-generated method stub				//배열 (많은 양의 데이터를 적은 코드로 처리하는것)
 		
-		int arrayInt[] = new int[10]; // arryInt[] 안에 0부터 9까지의 수를 담고있음
-		for (int i = 0; i<arrayInt.length; i++) {
-			System.out.println(i);
-	
+//		int arrayInt[] = new int[10]; // arryInt[] 안에 0부터 9까지의 수를 담고있음
+//		for (int i = 0; i<arrayInt.length; i++) {
+//			System.out.println(i);
+//		}
+//		System.out.println("-----1번째코드 끝 ----");
+//		
+//		
+//		int arrayInt2[] = {1,3,5,7,9};	
+//		for (int i = 0; i<arrayInt2.length; i++) { 	    // i=그냥 순서?
+//			System.out.println(i);
+//		}
+//		System.out.println("-----2번째코드 끝 ----");
+//		
+//		int arrayInt3[] = {1,3,5,7,9};
+//		for(int i = 0; i<arrayInt3.length; i++) { 		
+//			System.out.println(arrayInt3[i]);				//arrayInt3[i] i에 대입되는 숫자를 프린트
+//		}
+//		System.out.println("-----3번째코드 끝 ----");
+//		
+//		String arraystr[] = {"a","b","c","d","e"};
+//		for (int i = 0; i<arraystr.length; i++) {
+//			System.out.println(arraystr[i]);
+//	
+//		}
+//	}
+//}
+//
+//======================================================================================
+//		
+//		int[] scores = {83,90,87};
+//		
+//		System.out.println("scores[0]:"+ scores[0]);
+//		System.out.println("scores[1]:"+ scores[1]);
+//		System.out.println("scores[2]:"+ scores[2]);
+//
+//	
+//		int sum = 0;							// 위의 누적합산
+//		for(int i=0; i<3; i++) {
+//			sum += scores[i];
+//		}
+//		System.out.println("총합: " + sum);
+//		double avg = (double) sum / 3;
+//		System.out.println("평균: " + avg);
+//		
+//	}
+//}
+//=======================================================================================
+//		
+//		
+		int[] scores;
+		scores = new int[] { 83, 90, 87 };
+		int sum1 = 0;
+		for(int i=0; i<3; i++) {
+			sum1 += scores[i];
+			
 		}
-		
-		int arrayInt2[] = {1,3,5,7,9};	
-		for (int i = 0; i<arrayInt2.length; i++) { 	    // i=그냥 순서?
-			System.out.println(i);
-	
-		}
-		
-		int arrayInt3[] = {1,3,5,7,9};
-		for(int i = 0; i<arrayInt3.length; i++) { 		
-			System.out.println(arrayInt3[i]);				//arrayInt3[i] i에 대입되는 숫자를 프린트
-		}
-		
-		String arraystr[] = {"a","b","c","d","e"};
-		for (int i = 0; i<arraystr.length; i++) {
-			System.out.println(arraystr[i]);
-	
-		}
+		System.out.println("총합: " +sum1);
 	}
-	}
-	
+}
 
-
+		

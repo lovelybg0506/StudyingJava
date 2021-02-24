f34dc791b1567bb6c4b8ee9ea30a950db0e1c5d1
lovelybg0506 <lovelybg0506@gmail.com>
2021-02-24 17:33:44 +0900

    2021.02.24

---
+++ src/practice/Java9.java
@@ -0,0 +1,17 @@
+package practice;
+
+public class Java9 {
+	
+		
+	String a= "자바테스트" ;	//default 접근제한자  / test.java와 함께 작성함 / 같은 패키지 안에 있어야함
+	public String b= "자바" ; // public을 붙이면 같은 패키지가 아니어도 가능.
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+	
+	}
+
+}

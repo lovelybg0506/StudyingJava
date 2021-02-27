1289177b532dfaa724efe09bf1be1b572b6929f0
lovelybg0506 <lovelybg0506@gmail.com>
2021-02-27 21:03:28 +0900

    match test 0227

---
+++ src/practice/java12.java
@@ -0,0 +1,24 @@
+package practice;
+
+import java.util.Scanner;
+
+public class java12 {
+
+	public static void main(String[] args) {
+
+		int num = 12;
+		int input ;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("숫자를 입력하세요 : ");
+		input = sc.nextInt();
+		
+		if(input == num) {
+			System.out.println("맞습니다.");
+		}else {
+			System.out.println("틀립니다.");
+				
+			}
+		}
+		
+	}

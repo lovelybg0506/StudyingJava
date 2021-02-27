84f22c4e85445825a1de1a3af95b1d81d91f3391
lovelybg0506 <lovelybg0506@gmail.com>
2021-02-27 20:28:57 +0900

    2021-02-27

---
+++ src/practice/java10.java
@@ -0,0 +1,29 @@
+package practice;
+
+public class java10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//  내이름 유니코드
+
+		int a,b,c;
+		int d,e,f;
+		
+		
+		a='강'; // 44053
+		b='병'; // 48337
+		c='규'; // 44508
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		
+		
+//		d=44053;e=48337;f=44508;
+//		
+//		System.out.printf("%c%c%c",d,e,f);
+//		
+		
+		
+	}
+
+}
---
+++ src/practice/java11.java
@@ -0,0 +1,30 @@
+package practice;
+
+import java.util.Scanner;
+
+public class java11 {
+
+	public static void main(String[] args) {
+		//스캐너입력
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("문자를 입력하세요 ");
+		char moon = sc.nextLine().charAt(0);
+		long imoon = (long)moon;
+		
+		System.out.println(imoon);
+
+//		System.out.print("숫자를 입력하세요 ");
+//		int moon = sc.nextInt();
+//		char imoon = (char)moon;
+//		
+//		System.out.println(imoon);
+		
+		
+		
+		
+	}
+
+}

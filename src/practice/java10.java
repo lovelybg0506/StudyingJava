fe36ffd5c98f397e95c6c1f6dc55fc4e0ee044dc
lovelybg0506 <lovelybg0506@gmail.com>
2021-02-27 20:58:31 +0900

    0227failed test

--- src/practice/java10.java
+++ src/practice/java10.java
@@ -22,6 +22,19 @@ public class java10 {
 //		
 //		System.out.printf("%c%c%c",d,e,f);
 //		
+		//=========================================================================
+//		연습 실패작입니다 char를 써서그런지 숫자를 써도 출력됨. 없애고싶은데 아직 모르겠ㅇ다 
+//		Scanner sc = new Scanner(System.in);
+//		char f;
+//		System.out.print("값을 입력하세요");
+//		f = sc.nextLine().charAt(0);
+//		
+//		if(f==(char)f) {
+//			System.out.printf("문자 '%c'의 유니코드값은 '%d' 입니다",f,(long)f);
+//		}
+//		else {
+//			System.out.printf("문자가 아닙니다.");
+//		}
 		
 		
 	}

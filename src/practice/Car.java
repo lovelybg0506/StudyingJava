5d93b40184ee02d6dad81ee8a7d091f5732adb6f
lovelybg0506 <lovelybg0506@gmail.com>
2021-03-16 01:41:22 +0900

    2021-03-15

--- src/practice/Car.java
+++ src/practice/Car.java
@@ -10,12 +10,17 @@ public class Car {
 	}
 	
 	Car(String model){
-		this.model = model;
+//		this.model = model;
+//		this.color = null;
+//		this.maxSpeed = 0;
+		this(model,null,0);
 	}
 	
 	Car(String model, String color){
-		this.model = model;
-		this.color = color;
+//		this.model = model;
+//		this.color = color;
+//		this.maxSpeed = 0;
+		this(model,color,0);
 	}
 	
 	Car(String model, String color, int maxSpeed){
